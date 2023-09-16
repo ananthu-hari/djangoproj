ca593a0fced095a7f8520844b520f77dd1cfbc57
Ananthakrishnan T H <ananthakrishnan.th@gmail.com>
2023-09-16 13:19:16 +0530

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   EMS/models.py

--- EMS/models.py
+++ EMS/models.py
@@ -1,3 +1,6 @@
+from django.db import models
+
+# Create your models here.
 class Event(models.Model):
     event_title = models.CharField(max_length=120)
     location = models.CharField(max_length=40)
