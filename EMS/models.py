b2b47d04f3b8d61e3a8f20aabb8f256fa0b22b92
Ananthakrishnan T H <ananthakrishnan.th@gmail.com>
2023-09-16 13:16:08 +0530

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   EMS/models.py

--- EMS/models.py
+++ EMS/models.py
@@ -1,20 +1,23 @@
-from django.db import models
-
-# Create your models here.
 class Event(models.Model):
-    event_title=models.CharField(max_length=120)
-    location=models.CharField(max_length=40)
-    date=models.CharField(max_length=40)
-    description=models.TextField()
+    event_title = models.CharField(max_length=120)
+    location = models.CharField(max_length=40)
+    date = models.CharField(max_length=40)
+    description = models.TextField()
+
+    class Meta:
+        db_table = 'event'  # Specify the table name as 'event'
 
     def __str__(self):
         return self.event_title
-    
+
 class Registration(models.Model):
-    full_name=models.CharField(max_length=25)
-    email=models.EmailField(max_length=35)
-    ph_no=models.IntegerField()
-    event=models.ForeignKey(Event,on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=25)
+    email = models.EmailField(max_length=35)
+    ph_no = models.IntegerField()
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'registration'  # Specify the table name as 'registration'
 
     def __str__(self):
         return self.full_name
